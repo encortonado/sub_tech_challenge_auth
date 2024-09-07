6b4bbaced58ef106ce90c06afc0ebd808ad8ae3d
Lucas Bortolo <lucasbortolo3@gmail.com>
2024-09-06 21:31:50 -0300

    first commit

---
+++ src/main/java/br/com/fiap/tech/sub_tech_challenge_auth/SubTechChallengeAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTechChallengeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubTechChallengeAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/tech/sub_tech_challenge_auth/SubTechChallengeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.sub_tech_challenge_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubTechChallengeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
